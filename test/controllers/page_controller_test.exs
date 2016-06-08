b277da2939cab5931b610005594d6451dc80aefa
Rob van den Bogaard <rob@wetransfer.com>
2016-06-08 13:04:56 +0200

    Fix page controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElmBlogger.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Hello ElmBlogger!"
   end
 end
